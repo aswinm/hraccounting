f411ed78c24c5250cd81c8a8acc14dea7fe7ad60
aswinm <aswinmurugesh94@gmail.com>
2015-11-01 16:25:32 +0530

    Environment

--- config/environment.js
+++ config/environment.js
@@ -29,7 +29,7 @@ module.exports = function(environment) {
   };
 
   if (environment === 'development') {
-	  ENV.APP.API_HOST = 'https://hraccountingbackend.herokuapp.com/';
+	  ENV.APP.API_HOST = 'http://localhost:8000';
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
@@ -50,7 +50,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-	  ENV.APP.API_HOST = 'https://hraccountingbackend.herokuapp.com/';
+	  ENV.APP.API_HOST = 'https://hraccountingbackend.herokuapp.com';
 
   }
 
